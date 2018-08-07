47042b3a1f71d1f80463be79e910a06f093c4c4b
Yura Rizhniv <novocaine19@gmail.com>
2018-08-07 19:49:44 +0300

    prepared for deploy

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
@@ -30,6 +31,14 @@ app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('hi, babe'));
 
 app.use('/api/recipes', recipes);
+
+if ((process.env.NODE_ENV = 'production')) {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
